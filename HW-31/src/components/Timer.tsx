5745b813ae811eef90902177796f0ad97e581db3
Daniel Kleyman <denny_klm1@yahoo.com>
2023-03-05 16:42:06 +0200

    HW-31 rev

--- HW-31/src/components/Timer.tsx
+++ HW-31/src/components/Timer.tsx
@@ -1,17 +1,30 @@
 import React from "react"
-export const Timer: React.FC = () => {
-    const styles: React.CSSProperties = { backgroundColor: "lightblue", fontSize: "2em" }
-    setTimeout(tic, 10000)
-   const [time, setTime] = React.useState(new Date())
-    function tic() {// тик - это обытие внутри
-        if(styles.backgroundColor = "lightblue"){
-            styles.backgroundColor = "red"
-           } else styles.backgroundColor = "lightblue"
-       setTime(new Date())      
-    }
+export const Timer:React.FC=()=>
+{
+   
+    const styles: React.CSSProperties= {backgroundColor:"lightblue", fontSize:"2em", color: "red",
+    textAlign:"center"}
+    let stylesH2: React.CSSProperties= {fontSize:"2em",color: "red",textAlign:"center"}
+    let stylesH2New: React.CSSProperties= {fontSize:"2em",color: "blue",textAlign:"center"}
+    setTimeout(tic, 1000);
+    const [time, setTime] = React.useState(new Date());
+    function tic()
+    {
+       setTime(new Date())
+    } 
+    const intervalDuration = 10000;
 
+    React.useEffect(() => {
+      const interval = setInterval(() => {
+        setChanger((styleColor) => styleColor === stylesH2 ? stylesH2New : stylesH2);
+      }, intervalDuration);
+      return () => clearInterval(interval);
+    }, []);
+      
+    let [changer,setChanger] = React.useState(stylesH2);
+  
     return <div>
-        <h2>Current Time</h2>
-        <p style={styles}>{time.toLocaleTimeString()}</p>
+    <h2 className="text" style ={changer}> Current time </h2>
+    <p style={styles}>{time.toLocaleTimeString()}</p>
     </div>
-}
+    }
