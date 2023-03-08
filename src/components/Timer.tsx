db46cab951c7f44bf6ac9de41f36392a189e2586
Daniel Kleyman <denny_klm1@yahoo.com>
2023-03-08 19:51:10 +0200

    HW-32 rev

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,26 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import timeZones from "../time-zones";
 type Props = {
-    cityCountry:string;
+    cityCountry: string;
 }
-export const Timer:React.FC<Props> = ({cityCountry}) => {
-const styles: React.CSSProperties = {backgroundColor:"lightblue",
-fontSize: "2em"};
+export const Timer: React.FC<Props> = ({ cityCountry }) => {
+    const styles: React.CSSProperties = {
+        backgroundColor: "lightblue",
+        fontSize: "2em",
+        textAlign: "center",
+        marginLeft: "2vw"
+    };
+    const stylesH2: React.CSSProperties = {
+        marginLeft: "2vw"
+    };
 
-const [time, setTime] = useState<Date>(new Date());
+    const [time, setTime] = useState<Date>(new Date());
+    let timeResult: string;
+    let index: number = timeZones.findIndex(getIndex);
 
-function tic() {
-    setTime(new Date());    
-}
+    useEffect(() => {
+        const interval = setInterval(tic, 2000);
+        console.log("useEffect");
+        return () => clearInterval(interval);
+    }, [])
 
-useEffect(() => {
-    const interval = setInterval(tic, 2000);
-    console.log("useEffect");
-    return () => clearInterval(interval);
-}, [])
-
+    function getIndex(elm: object, ind: number) {
+        const str = JSON.stringify(elm);
+        if (str.includes(cityCountry)) {
+            return ind
+        }
+    }
+    if (index == -1) {
+        timeResult = time.toLocaleTimeString();
+    } else {
+        timeResult = time.toLocaleTimeString(undefined,
+            { timeZone: timeZones[index].name })
+    }
+    function tic() {
+        setTime(new Date());
+    }
     return <div>
-        <h2 >Current Time in {cityCountry}</h2>
-        <p style={styles}>{time.toLocaleTimeString(undefined, {timeZone:timeZones[2].name})}</p>
+        <h2 style={stylesH2}>Current Time in {cityCountry}</h2>
+        <p style={styles}>{timeResult}</p>
     </div>
 }
