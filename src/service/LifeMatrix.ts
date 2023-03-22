71e8390e7ed59ace12d401a85af823ae27355185
Daniel Kleyman <denny_klm1@yahoo.com>
2023-03-22 18:50:17 +0200

    HW-36 rev

--- src/service/LifeMatrix.ts
+++ src/service/LifeMatrix.ts
@@ -1,14 +1,37 @@
+import { checkPrimeSync } from "crypto";
 import { getRandomMatrix } from "../util/random";
 
 export default class LifeMatrix {
-    constructor(private _numbers: number[][]){}
-        get numbers() {
-            return this._numbers;
-        }
-    
-    nextStep(): number[][]{
-        //fix in homework #36
-        this._numbers = getRandomMatrix(this._numbers.length, this._numbers.length, 0, 1);
+    constructor(private _numbers: number[][]) { }
+    get numbers() {
         return this._numbers;
+    }
+
+    nextStep(): number[][] {
+        return this._numbers = this._numbers.map((row, i) => {
+            return row.map((value, j) => {
+                let check = (row[j - 1] || 0) + (row[j + 1] || 0) +
+                    (this._numbers[i - 1]?.[j - 1] || 0) + (this._numbers[i - 1]?.[j] || 0) +
+                    (this._numbers[i - 1]?.[j + 1] || 0) + (this._numbers[i + 1]?.[j - 1] || 0) +
+                    (this._numbers[i + 1]?.[j] || 0) + (this._numbers[i + 1]?.[j + 1] || 0);
+                return this.checkFn(check, value);
+            }
+            );
+        });
+
+    }
+    checkFn(check: number, value: number): number {
+        let checkValue: number = value;
+        let checkRes: number = check;
+        if (checkValue == 1 && checkRes < 2) {
+            return 0;
+        } else if (checkValue == 1 && (2 <= checkRes && checkRes <= 3)) {
+            return 1;
+        } else if (checkValue == 1 && checkRes > 3) {
+            return 0;
+        } else if (checkValue == 0 && checkRes === 3) {
+            return 1;
+        }
+        return checkValue;
     }
 }
