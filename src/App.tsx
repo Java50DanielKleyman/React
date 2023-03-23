dd6f1a6b15b51c2f956ee8337c23eae59e4c38e2
Daniel Kleyman <denny_klm1@yahoo.com>
2023-03-23 17:16:18 +0200

    HW-36 rev

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Life } from "./components/Life";
 import { StartButton } from "./components/StartButton"
 import { StopButton } from "./components/StopButton"
@@ -7,15 +7,23 @@ import {ResetButton} from "./components/ResetButton"
 function App() {
   const [isStart, setIsStart] = useState(false)
   const [isReset, setReset] = useState(false)
+  const [lifeKey, setLifeKey] = useState(0)
   function startGame(): void {
     setIsStart(true);    
   }
   function stopGame(): void {
     setIsStart(false);    
   }
-  function resetGame():void {
-       
+  function resetGame():void {    
+       setReset(true);
+       setLifeKey(lifeKey + 1);
   }
+  useEffect(() => {
+    if (isReset) {
+      setReset(false);
+    }
+  }, [isReset])
+ 
   return <>
     {!isStart && <div style={{
       display: "flex", justifyContent: "center", marginTop: "35vh",
@@ -23,11 +31,14 @@ function App() {
     }}><StartButton onClick={startGame} /></div>}
     {isStart && <div style={{
       display: "flex", justifyContent: "center", marginTop: "3vh"
-    }}><StopButton onClick={stopGame} /></div>}    
+    }}><StopButton onClick={stopGame} /></div>} 
+       {isStart && <div style={{
+      display: "flex", justifyContent: "center", marginTop: "3vh"
+    }}><ResetButton onClick={resetGame} /></div>} 
     {isStart && <div style={{
       display: "flex", justifyContent: "center",
       alignItems: "center"
-    }}><Life /></div>}
+    }}><Life key={lifeKey}/></div>}
   </>
 }
 
