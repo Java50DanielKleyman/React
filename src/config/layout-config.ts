5d1d9bd53f990d4cfd841e427cbe8bec7904b155
Daniel Kleyman <denny_klm1@yahoo.com>
2023-04-02 18:32:54 +0300

    HW-39 rev

--- src/config/layout-config.ts
+++ src/config/layout-config.ts
@@ -1,11 +1,11 @@
 import { RouteType } from "../model/RouteType";
 
 export const routes: RouteType[] = [
-    {path: '/', label: 'Home'},
-    {path: '/customers', label: 'Customers'},
-    {path: '/shoppingcart', label: 'Shopping Cart'},
-    {path: '/orders', label: 'Orders'},
-    {path: '/products', label: 'Products'},
-     {path: '/login', label: 'Login'},
-     {path: '/logout', label: 'Logout'}
+    {path: '/', label: 'Home',  authenticated: true, admin: true},
+    {path: '/customers', label: 'Customers', admin: true},
+    {path: '/shoppingcart', label: 'Shopping Cart', authenticated: true, admin: true},
+    {path: '/orders', label: 'Orders', authenticated: true, admin: true},
+    {path: '/products', label: 'Products', always: true},
+     {path: '/login', label: 'Login', no_authenticated: true},
+     {path: '/logout', label: 'Logout', authenticated: true, admin: true}
 ]
