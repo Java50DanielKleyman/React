5dfaa0565198522d890d819df41d10fab3fe3f7b
Daniel Kleyman <denny_klm1@yahoo.com>
2023-04-19 15:43:55 +0300

    HW-42 rev

--- src/ProductsService.test.ts
+++ src/ProductsService.test.ts
@@ -1,6 +1,5 @@
 import { productsService } from "./config/products-service-config"
 import productsConfig from "./config/products-config.json"
-import { getCountFromServer } from "firebase/firestore";
 import { getRandomElement } from "./util/random";
 
 const categoriesArray = productsConfig.map(pc => {
@@ -29,15 +28,3 @@ test("add category", () => {
         .then(res => expect(res).toBeFalsy())
 })
 
-
-// test("setProducts test", () => {
-//     productsService.setProducts().then(count => {
-//         expect(count).toEqual(productsConfig.length);
-//     })
-// })
-// test ("category bread exists", () => {
-//     productsService.isCategoryExist("bread").then(res => expect(res).toBeTruthy())
-// })
-// test ("category kukureku doesnt exists", () => {
-//     productsService.isCategoryExist("bread").then(res => expect(res).toBeFalsy());
-// }) 
