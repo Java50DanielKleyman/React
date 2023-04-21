96031ba8f88983a7074c41945ceeee0276215dc2
Daniel Kleyman <denny_klm1@yahoo.com>
2023-04-21 15:04:58 +0300

    HW-42 rev

--- src/ProductsService.test.ts
+++ src/ProductsService.test.ts
@@ -1,51 +1,76 @@
+/**
+ * @jest-environment node
+ */
+
+// ATTENTION!!! The comment above is critical to run Firestore tests.
+//              It avoids conflict of jsdom and node test frameworks.
+// See: https://stackoverflow.com/questions/70327245/cant-write-firestore-tests-because-of-firestore-9-6-1-internal-assertion-fai
+
 import { productsService } from "./config/products-service-config"
 import productsConfig from "./config/products-config.json"
-import { getRandomElement } from "./util/random";
+test("setProducts test", () => 
+    productsService.setProducts().then((count: any) => {
+        expect(count).toEqual(productsConfig.length-1);
+    })
+)
+test ("category bread exists", () => 
+    productsService.isCategoryExist("bread")
+    .then((res: any) => expect(res).toBeTruthy())
+)
+test ("category kukureku doesn't exist", () =>
+    productsService.isCategoryExist("kukureku")
+    .then((res: any) => expect(res).toBeFalsy())
+)
 
-const categoriesArray: string[] = [];
-productsConfig.forEach(pc => {
-    const category = pc.name.split("-")[0];
-    if (!categoriesArray.includes(category)) {
-        categoriesArray.push(category);
-    }
-})
-test ("category bread exists", () => {
-    productsService.isCategoryExist("sdsdssds")
-    .then(res => expect(res).toBeTruthy());
-})
-test("Random category exists", () => {
-    productsService.getCategoriesCount()
-        .then(res => {
-            if (res == 0) {
-                return;
-            } else {
-                const randomCategory = getRandomElement(categoriesArray);
-                productsService.isCategoryExist("randomCategory")
-                    .then(res => expect(res).toBeTruthy());
-            }
-        });
-});
-
+// import { productsService } from "./config/products-service-config"
+// import productsConfig from "./config/products-config.json"
+// import { getRandomElement } from "./util/random";
+// /**
+//  * @jest-environment node
+//  */
+// const categoriesArray: string[] = [];
+// productsConfig.forEach(pc => {
+//     const category = pc.name.split("-")[0];
+//     if (!categoriesArray.includes(category)) {
+//         categoriesArray.push(category);
+//     }
+// })
+// test ("category bread exists", () => {
+//     productsService.isCategoryExist("sdsdssds")
+//     .then(res => expect(res).toBeTruthy());
+// })
 // test("Random category exists", () => {
-//     const randomCategory = getRandomElement(categoriesArray);
-//     productsService.isCategoryExist(randomCategory)
+//     productsService.getCategoriesCount()
+//         .then(res => {
+//             if (res == 0) {
+//                 return;
+//             } else {
+//                 const randomCategory = getRandomElement(categoriesArray);
+//                 productsService.isCategoryExist("randomCategory")
+//                     .then(res => expect(res).toBeTruthy());
+//             }
+//         });
+// });
+
+// // test("Random category exists", () => {
+// //     const randomCategory = getRandomElement(categoriesArray);
+// //     productsService.isCategoryExist(randomCategory)
+// //         .then(res => expect(res).toBeFalsy())
+// // })
+// test("All categories exist", () => {
+//     const arrayOfPromises = categoriesArray.map(catName => {
+//         return productsService.isCategoryExist(catName);
+//     })
+//     Promise.all(arrayOfPromises).then((res) =>
+//         expect(res.every((elm) => elm)).toBeFalsy())
+// })
+// test("Remove category", () => {
+//     productsService.removeCategory("bread").then(() =>
+//         productsService.isCategoryExist("bread"))
+//         .then(res => expect(res).toBeTruthy());
+// })
+// test("add category", () => {
+//     productsService.addCategory({ name: "beer" })
+//         .then(() => productsService.isCategoryExist("beer"))
 //         .then(res => expect(res).toBeFalsy())
 // })
-test("All categories exist", () => {
-    const arrayOfPromises = categoriesArray.map(catName => {
-        return productsService.isCategoryExist(catName);
-    })
-    Promise.all(arrayOfPromises).then((res) =>
-        expect(res.every((elm) => elm)).toBeFalsy())
-})
-test("Remove category", () => {
-    productsService.removeCategory("bread").then(() =>
-        productsService.isCategoryExist("bread"))
-        .then(res => expect(res).toBeTruthy());
-})
-test("add category", () => {
-    productsService.addCategory({ name: "beer" })
-        .then(() => productsService.isCategoryExist("beer"))
-        .then(res => expect(res).toBeFalsy())
-})
-
