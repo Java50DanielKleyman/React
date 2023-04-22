e662c495f94c4bb6badbd1719c782248db83d000
Daniel Kleyman <denny_klm1@yahoo.com>
2023-04-22 17:48:46 +0300

    HW-43 rev

--- src/components/pages/ProductsClient.tsx
+++ src/components/pages/ProductsClient.tsx
@@ -1,35 +1,38 @@
-import { Typography, Box, Avatar } from "@mui/material"
+import { Typography, Box, Avatar, CardContent } from "@mui/material"
 import {DataGrid, GridColDef} from "@mui/x-data-grid"
 import { ProductType } from "../../model/ProductType"
 import { useSelector } from "react-redux"
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
 
 export const ProductsClient: React.FC = () => {
     const products: ProductType[] =
      useSelector<any, ProductType[]>(state => state.productsState.products);
     
-    return <Box sx={{ width: "80vw", height: "80vh"  }}>   
+    return <Box sx={{ width: "80vw", height: "80vh" }}>
     <Grid container spacing={2} sx={{ marginTop: "20px", marginLeft: "8vw" }}>
       {products.map((product) => (
-        <Grid item xs={3} key={product.id} >
-          <Box sx={{ padding: "10px", border: "1px solid gray" }}>
+        <Grid item xs={3} key={product.id}>
+          <Card sx={{ height: "100%" }}>
             <Avatar
               src={`images/${product.image}`}
               sx={{ width: "100%", height: "24vh" }}
             />
-            <Typography variant="h6" sx={{ marginTop: "10px", display: "flex", justifyContent: "center", alignItems: "center"  }}>
-              {product.title}
-            </Typography>
-            <Typography variant="subtitle1" sx={{ color: "gray", display: "flex", justifyContent: "center", alignItems: "center" }}>
-              {product.category}
-            </Typography>
-            <Typography variant="subtitle1" sx={{ color: "gray", display: "flex", justifyContent: "center", alignItems: "center" }}>
-              {product.unit}
-            </Typography>
-            <Typography variant="body1" sx={{ marginTop: "10px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-              {product.cost } ₪
-            </Typography>
-          </Box>
+            <CardContent sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+              <Typography variant="h6" sx={{ marginTop: "10px" }}>
+                {product.title}
+              </Typography>
+              <Typography variant="subtitle1" sx={{ color: "gray" }}>
+                {product.category}
+              </Typography>
+              <Typography variant="subtitle1" sx={{ color: "gray" }}>
+                {product.unit}
+              </Typography>
+              <Typography variant="body1" sx={{ marginTop: "10px" }}>
+                {product.cost} ₪
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       ))}
     </Grid>
