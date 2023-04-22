3ad90b0ca362afe025c8728e6a31fbe58e4a1fd3
Daniel Kleyman <denny_klm1@yahoo.com>
2023-04-22 17:34:56 +0300

    HW-43 rev

--- src/components/pages/ProductsClient.tsx
+++ src/components/pages/ProductsClient.tsx
@@ -8,26 +8,26 @@ export const ProductsClient: React.FC = () => {
     const products: ProductType[] =
      useSelector<any, ProductType[]>(state => state.productsState.products);
     
-    return <Box sx={{ width: "80vw", height: "80vh" }}>   
-    <Grid container spacing={2} sx={{ marginTop: "20px", justifyContent: "center" }}>
+    return <Box sx={{ width: "80vw", height: "80vh"  }}>   
+    <Grid container spacing={2} sx={{ marginTop: "20px", marginLeft: "8vw" }}>
       {products.map((product) => (
-        <Grid item xs={3} key={product.id}>
+        <Grid item xs={3} key={product.id} >
           <Box sx={{ padding: "10px", border: "1px solid gray" }}>
             <Avatar
               src={`images/${product.image}`}
-              sx={{ width: "100%", height: "150px" }}
+              sx={{ width: "100%", height: "24vh" }}
             />
-            <Typography variant="h6" sx={{ marginTop: "10px" }}>
+            <Typography variant="h6" sx={{ marginTop: "10px", display: "flex", justifyContent: "center", alignItems: "center"  }}>
               {product.title}
             </Typography>
-            <Typography variant="subtitle1" sx={{ color: "gray" }}>
+            <Typography variant="subtitle1" sx={{ color: "gray", display: "flex", justifyContent: "center", alignItems: "center" }}>
               {product.category}
             </Typography>
-            <Typography variant="subtitle1" sx={{ color: "gray" }}>
+            <Typography variant="subtitle1" sx={{ color: "gray", display: "flex", justifyContent: "center", alignItems: "center" }}>
               {product.unit}
             </Typography>
-            <Typography variant="body1" sx={{ marginTop: "10px" }}>
-              {product.cost}
+            <Typography variant="body1" sx={{ marginTop: "10px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+              {product.cost } ₪
             </Typography>
           </Box>
         </Grid>
