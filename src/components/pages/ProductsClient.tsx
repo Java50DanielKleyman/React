9ad57866b41d255558b72b0b7b84e588f4834807
Daniel Kleyman <denny_klm1@yahoo.com>
2023-04-23 17:52:35 +0300

    HW-43 rev

--- src/components/pages/ProductsClient.tsx
+++ src/components/pages/ProductsClient.tsx
@@ -10,7 +10,14 @@ export const ProductsClient: React.FC = () => {
         useSelector<any, ProductType[]>(state => state.productsState.products);
 
     return <Box sx={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Grid container spacing={2} sx={{ marginTop: "20px", marginLeft: "5vw", marginRight: "5vw" }}>
+        <Grid container spacing={2} sx={{   maxWidth: "1500px",
+  margin: "0 auto",
+  marginTop: "20px",
+  '@media (max-width: 600px)': {
+    maxWidth: "100%",
+    paddingLeft: "10px",
+    paddingRight: "10px",
+  }  }}>
             {products.map((product) => (
                 <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
                     <Card sx={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "center" }}>
