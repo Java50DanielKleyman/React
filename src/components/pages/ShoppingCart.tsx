ce66b27f95ab80a2a23ece6b475945ec59f4e0ef
Daniel Kleyman <denny_klm1@yahoo.com>
2023-04-29 18:47:52 +0300

    HW-44 rev

--- src/components/pages/ShoppingCart.tsx
+++ src/components/pages/ShoppingCart.tsx
@@ -1,4 +1,71 @@
+import { Avatar, Box } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { ProductType } from "../../model/ProductType";
+import { ShoppingProductDataType } from "../../model/ShoppingProductDataType";
+import { ShoppingProductType } from "../../model/ShoppingProductType";
+
 export const ShoppingCart: React.FC = () => {
-    return <h2>ShoppingCart loadded</h2>
-    
+    const products: ProductType[] =
+        useSelector<any, ProductType[]>(state => state.productsState.products);
+    const shopping: ShoppingProductType[] =
+        useSelector<any, ShoppingProductType[]>(state => state.shoppingState.shopping);
+
+    function getTableData(): ShoppingProductDataType[] {
+        const shoppingData = shopping.map((item) => {
+            const product = products.find((p) => p.id === item.id);
+            if (product) {
+                return {
+                    ...product,
+                    count: item.count,
+                    totalCost: product.cost * item.count,
+                };
+            }
+            return null;
+        });
+
+        return shoppingData.filter((item) => item !== null) as ShoppingProductDataType[];
+    }
+    const tableData = useMemo(() => getTableData(), [products, shopping]);
+    const columns: GridColDef[] = [
+        {
+            field: "image",
+            headerName: '',
+            flex: 0.3,
+            renderCell: (params) => (
+                <Avatar
+                    src={`images/${params.value}`}
+                    sx={{ width: "90%", height: "80px" }}
+                />
+            ),
+            align: "center",
+            headerAlign: "center"
+        },
+        {
+            field: "title",
+            headerName: 'Title',
+            flex: 0.8,
+            align: "center",
+            headerAlign: "center"
+        },
+        { field: "category", headerName: "Category", flex: 0.5 },
+        { field: "unit", headerName: "Unit", flex: 0.4 },
+        { field: "cost", headerName: "Cost (ILS)", flex: 0.3 },
+        { field: "count", headerName: "Count", flex: 0.3 },
+        { field: "totalCost", headerName: "Total Cost (ILS)", flex: 0.4 },
+    ];
+
+    return (
+        <Box sx={{ width: "100vw", display: "flex", justifyContent: "center" }}>
+            <Box sx={{ width: "80vw", height: "80vh" }}>
+                <DataGrid
+                    columns={columns}
+                    rows={getTableData()}
+                    getRowHeight={() => 'auto'}
+                />
+            </Box>
+        </Box>
+    );
+
 }
---
+++ src/model/ShoppingProductDataType.ts
@@ -0,0 +1,3 @@
+import { ProductType } from "./ProductType";
+
+export type ShoppingProductDataType = ProductType & {count: number, totalCost: number}
