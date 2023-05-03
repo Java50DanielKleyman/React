56a3a0643ef75929ec393caa8c57ea19e8e6bd0c
Daniel Kleyman <denny_klm1@yahoo.com>
2023-05-03 15:40:29 +0300

    HW-45 rev

--- src/components/pages/ProductsAdmin.tsx
+++ src/components/pages/ProductsAdmin.tsx
@@ -1,5 +1,5 @@
 import { Typography, Box, Avatar, Snackbar, Alert } from "@mui/material"
-import {DataGrid, GridActionsCellItem, GridColDef} from "@mui/x-data-grid"
+import { DataGrid, GridActionsCellItem, GridColDef } from "@mui/x-data-grid"
 import { ProductType } from "../../model/ProductType"
 import { useSelector } from "react-redux"
 import { useRef, useState } from "react"
@@ -12,42 +12,46 @@ export const ProductsAdmin: React.FC = () => {
     const [open, setOpen] = useState<boolean>(false);
     const alertMessage = useRef<string>('');
     const products: ProductType[] =
-     useSelector<any, ProductType[]>(state => state.productsState.products);
+        useSelector<any, ProductType[]>(state => state.productsState.products);
     const columns: GridColDef[] = [
-        {field:"image", headerName: '', flex: 0.3,
-         renderCell: (params) => <Avatar src={`images/${params.value}`} 
-         sx={{width: "90%", height: "80px"}}/>, align: "center", headerAlign: "center"},
-        {field: "title", headerName: 'Title', flex: 0.8, align: "center", headerAlign: "center"},
-        {field: "category", headerName: "Category", flex: 0.5},
-        {field: "unit", headerName: "Unit", flex: 0.4},
-        {field: "cost", headerName: "Cost (ILS)", flex: 0.3},
-        {field: 'actions', type: 'actions', flex: 0.1, getActions: (params) => [
-            <GridActionsCellItem label="remove" icon={<Delete></Delete>}
-             onClick={async () => await 
-                productsService.removeProduct(params.id as string)}/>
-        ]}
+        {
+            field: "image", headerName: '', flex: 0.3,
+            renderCell: (params) => <Avatar src={`images/${params.value}`}
+                sx={{ width: "90%", height: "80px" }} />, align: "center", headerAlign: "center"
+        },
+        { field: "title", headerName: 'Title', flex: 0.8, align: "center", headerAlign: "center" },
+        { field: "category", headerName: "Category", flex: 0.5 },
+        { field: "unit", headerName: "Unit", flex: 0.4 },
+        { field: "cost", headerName: "Cost (ILS)", flex: 0.3, editable: true, type: 'number' },
+        {
+            field: 'actions', type: 'actions', flex: 0.1, getActions: (params) => [
+                <GridActionsCellItem label="remove" icon={<Delete></Delete>}
+                    onClick={async () => await
+                        productsService.removeProduct(params.id as string)} />
+            ]
+        }
     ]
     async function updateCount(newRow: any): Promise<any> {
-        const rowData: ShoppingDataType = newRow;
-        if (rowData.count < 1) {
-            throw 'count must be greater than 0'
+        const rowData: ProductType = newRow;
+        if (rowData.cost < 1 || rowData.cost > 1000) {
+            throw 'cost must be more than 1 and less than 1000 NIS'
         }
-        await ordersService.addShoppingProduct(authUser,
-            rowData.id!, { id: rowData.id!, count: rowData.count })
+        await productsService.addProduct(rowData)
         return newRow;
     }
-    return <Box sx={{width: "100vw",display: "flex", justifyContent:"center"}}>
-        <Box sx={{width: "80vw",height: "80vh"}}>
-        <DataGrid columns={columns} rows={products} getRowHeight={() => 'auto'}
-        onProcessRowUpdateError={(error) => {
-            alertMessage.current = error;
-            setOpen(true)
-         }}/>
-        <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
-            <Alert severity="error" sx={{ width: '30vw', fontSize: '1.5em' }}>
-                {alertMessage.current}
-            </Alert>
-        </Snackbar>
-    </Box>
+    return <Box sx={{ width: "100vw", display: "flex", justifyContent: "center" }}>
+        <Box sx={{ width: "80vw", height: "80vh" }}>
+            <DataGrid columns={columns} rows={products} getRowHeight={() => 'auto'}
+                processRowUpdate={updateCount}
+                onProcessRowUpdateError={(error) => {
+                    alertMessage.current = error;
+                    setOpen(true)
+                }} />
+            <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+                <Alert severity="error" sx={{ width: '30vw', fontSize: '1.5em' }}>
+                    {alertMessage.current}
+                </Alert>
+            </Snackbar>
+        </Box>
     </Box>
 }
